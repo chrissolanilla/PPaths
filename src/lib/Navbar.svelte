edfaf66feb77b1461da008483c020c24df9ad7c4
Chris <chrissolanilla@gmail.com>
2024-01-20 19:48:50 -0500

    test

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,24 @@
+<!-- Navbar.svelte in the lib directory -->
+<script lang='ts'>
+    // Function to handle scrolling to a specific section
+    function scrollToSection(sectionId:any) {
+      const section = document.querySelector(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  </script>
+  
+  <nav class="navbar bg-yellow-100 flex items-center">
+    <div class="flex items-center justify-center">
+      <a class="btn btn-ghost normal-case text-xl font-bold">Pregnancy Paths</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal p-0">
+        <li class="font-bold"><a on:click={() => scrollToSection('#statistics')}>Statistics</a></li>
+        <li class="font-bold"><a on:click={() => scrollToSection('#testimonials')}>Testimonials</a></li>
+        <!-- Add more menu items here -->
+      </ul>
+    </div>
+  </nav>
+  
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang='ts'>
+    import '../styles/main.css'
+    import '../app.css'
+
+</script>
+
+<slot></slot>
